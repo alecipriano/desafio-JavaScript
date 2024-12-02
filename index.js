f45463034f70d63be9ac54a7bdea959a2d3b7939
alecipriano <alleciprianos@gmail.com>
2024-12-02 14:50:58 -0300

    Lista de herois atualizada

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-let nome = [
-   ["Ferro", 977],
-   ["Bronze", 1258],
-   ["Prata", 3500],
-   ["Ouro", 6950],
-   ["Platina", 7589],
-   ["Ascendente", 8965],
-   ["Imortal", 9812],
-   ["Radiante", 10951],   
-]
+let herois = [
+   ["Batman", 977],
+   ["Homen Aranha", 1258],
+   ["Homem De Ferro", 3500],
+   ["Hulk", 6950],
+   ["Capitao America", 7589],
+   ["Doutor Estranho", 8965],
+   ["SuperMan", 9812],
+   ["Thor", 10951],   
+];
 
-console.log(nome[1]); 
+for (let i = 0; i < herois.length; i++) {
+   let nome = herois[i][0];
+   let xp = herois[i][1];
+   let nivel = "";
+
+   if (xp < 1000) {
+       nivel = "Ferro";
+   } else if (xp <= 2000) {
+       nivel = "Bronze";
+   } else if (xp <= 5000) {
+       nivel = "Prata";
+   } else if (xp <= 7000) {
+       nivel = "Ouro";
+   } else if (xp <= 8000) {
+       nivel = "Platina";
+   } else if (xp <= 9000) {
+       nivel = "Ascendente";
+   } else if (xp <= 10000) {
+       nivel = "Imortal";
+   } else {
+       nivel = "Radiante";
+   }
+
+   console.log(`O Herói de nome ${nome} possui atualmente o xp no valor de ${xp} portanto seu nivel é ${nivel}.`);
+
+
+}
