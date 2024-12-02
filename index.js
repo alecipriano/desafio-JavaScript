96977fb2e22d932e8dd1cbe6688ed397f44bf2cc
alecipriano <alleciprianos@gmail.com>
2024-12-02 13:41:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let nome = [
+   ["Ferro", 977],
+   ["Bronze", 1258],
+   ["Prata", 3500],
+   ["Ouro", 6950],
+   ["Platina", 7589],
+   ["Ascendente", 8965],
+   ["Imortal", 9812],
+   ["Radiante", 10951],   
+]
+
+console.log(nome[1]); 
